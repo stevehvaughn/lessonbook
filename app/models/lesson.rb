40b53ae9fd42fb4c0a8bc055a9443454709181a4
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-09-02 14:45:01 -0600

    Users relationship works with the self-join. Seed data added for Students and Teachers"

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,4 @@
 class Lesson < ApplicationRecord
-  belongs_to :user
+  belongs_to :student, foreign_key: "student_id", class_name: "User"
   has_many :comments, as: :commentable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+   
     has_many :students, class_name: "User", foreign_key: "teacher_id"
-    belongs_to :teacher, class_name: "User"
+    belongs_to :teacher, class_name: "User", optional: true
+    
+    has_many :lessons
+    has_many :practice_logs, foreign_key: "student_id"
 end
