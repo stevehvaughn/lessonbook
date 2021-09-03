d194da901a9a3e6703db37168a1e4aa331be4de1
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-09-02 19:05:25 -0600

    made login folder with css file, added basic form to test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <h1>YO DAWG</h1>
+      <Login />
     </div>
   );
 }
--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -22,7 +22,7 @@ export const Button = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return (
-        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type}>
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={onClick} type={type} id='navbar-main-signup'>
             {children}
         </button>
     )
