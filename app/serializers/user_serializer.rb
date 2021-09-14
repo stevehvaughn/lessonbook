f2de0424d8d769aa52b665676f60d886dd9a218d
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-09-14 13:46:20 -0600

    added year_in_school back to users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :combined_name, :email, :username, :picture_url, :lessons, :lesson_day, :lesson_time
+  attributes :id, :first_name, :last_name, :combined_name, :email, :username, :picture_url, :year_in_school, :lesson_day, :lesson_time, :lessons
 
   has_many :students, serializer: UserStudentSerializer
   belongs_to :teacher, serializer: UserTeacherSerializer
