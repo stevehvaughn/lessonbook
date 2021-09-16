3b8f670554feebf2f04d12dbc903ea745029e667
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-09-16 16:38:28 -0600

    final push before presentation

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   get '/teachers', to: 'users#teachers'
   get '/users-students', to: 'users#users_students'
+
+  get '*path', to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
