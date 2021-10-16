935d1823eb294eb029c1b1aae11629bb2ce7cfed
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-10-16 09:53:24 -0600

    added avatar for student view

--- client/src/components/StudentView/Student.js
+++ client/src/components/StudentView/Student.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import Avatar from 'react-avatar';
 
 const Student = () => {
     const student = useSelector(state => state.user)
     
     return (
         <div>
-            <h1>Welcome {student.first_name}!</h1>
+             <div className='teacher-header'>
+                { student.picture_url ? <img className="avatar-picture" src={student.picture_url} alt="student_picture"></img> : <Avatar round='50%' name={student.combined_name} /> }
+                <h1 id="teacher-header-text">Welcome {student.first_name}!</h1>
+            </div>
         </div>
     )
 }
