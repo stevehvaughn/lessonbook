c42f0840bee6c59ebe05b669bb1c7af5973382c7
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-10-18 11:27:23 -0600

    changed comma to dash for lessons map

--- client/src/components/StudentView/Student.js
+++ client/src/components/StudentView/Student.js
@@ -71,7 +71,7 @@ const Student = () => {
                     {upcomingLessons.map(lesson => { return (
                         <>
                         <li id={lesson.id} key={lesson.id} className='single-lesson' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={renderFullLesson}>
-                            {getDayOfWeek(lesson.date)}, {getFormattedDate(lesson.date)}
+                            {getDayOfWeek(lesson.date)} - {getFormattedDate(lesson.date)}
                         </li>
                         {isHovering === true && lesson.id === parseInt(hoveringID) ? <p className='hover-text' style={{ left: parseInt(pageX), top: parseInt(pageY) + 10 }}>{lesson.objective}<br/><br/>{lesson.assignment}</p> : null}
                         </>
@@ -84,7 +84,7 @@ const Student = () => {
                     {priorLessons.map(lesson => { return (
                         <>
                         <li id={lesson.id} key={lesson.id} className='single-lesson' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={renderFullLesson}>
-                            {getDayOfWeek(lesson.date)}, {getFormattedDate(lesson.date)}
+                            {getDayOfWeek(lesson.date)} - {getFormattedDate(lesson.date)}
                         </li>
                         {isHovering === true && lesson.id === parseInt(hoveringID) ? <p className='hover-text' style={{ left: parseInt(pageX), top: parseInt(pageY) + 10 }}>{lesson.objective}<br/><br/>{lesson.assignment}</p> : null}
                         </>
